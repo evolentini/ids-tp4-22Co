289ff33705585e3d681ef7a9f726820d817a7097
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2024-09-12 21:40:58 -0300

    Correcciones de formato

--- test/test_leds.c
+++ test/test_leds.c
@@ -4,38 +4,38 @@
 static uint16_t ledsVirtuales;
 
 void setUp(void) {
-   Leds_Create(&ledsVirtuales);
+    Leds_Create(&ledsVirtuales);
 }
 
 void tearDown(void) {
 }
 
 void test_LedsOffAfterCreate(void) {
-   uint16_t ledsVirtuales = 0xFFFF;
-   Leds_Create(&ledsVirtuales);
-   TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
+    uint16_t ledsVirtuales = 0xFFFF;
+    Leds_Create(&ledsVirtuales);
+    TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
 }
 
 void test_TurnOnLedOne(void) {
-   Leds_TurnOn(1);
-   TEST_ASSERT_EQUAL_HEX16(1, ledsVirtuales);
+    Leds_TurnOn(1);
+    TEST_ASSERT_EQUAL_HEX16(1, ledsVirtuales);
 }
 
 void test_TurnOffLedOne(void) {
-   Leds_TurnOn(1);
-   Leds_TurnOff(1);
-   TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
+    Leds_TurnOn(1);
+    Leds_TurnOff(1);
+    TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
 }
 
 void test_TurnOnMultipleLeds(void) {
-   Leds_TurnOn(8);
-   Leds_TurnOn(9);
-   TEST_ASSERT_EQUAL_HEX16(0x0180, ledsVirtuales);
+    Leds_TurnOn(8);
+    Leds_TurnOn(9);
+    TEST_ASSERT_EQUAL_HEX16(0x0180, ledsVirtuales);
 }
 
 void test_TurnOffAnyLed(void) {
-   Leds_TurnOn(8);
-   Leds_TurnOn(9);
-   Leds_TurnOff(8);
-   TEST_ASSERT_EQUAL_HEX16(0x0100, ledsVirtuales);
+    Leds_TurnOn(8);
+    Leds_TurnOn(9);
+    Leds_TurnOff(8);
+    TEST_ASSERT_EQUAL_HEX16(0x0100, ledsVirtuales);
 }
